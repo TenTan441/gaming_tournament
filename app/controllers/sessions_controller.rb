3d1d4b6b2e08c5786339c9b5f2db416463d37377
Yoshiki <myutos44@gmail.com>
2019-08-12 15:41:21 +0000

    Finish editing-users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       log_in user
       ActiveRecord::Type::Boolean.new.cast(params[:session][:remember_me]) ? remember(user) : forget(user)
-      redirect_to user
+      redirect_back_or user
     else
       flash.now[:danger] = '認証に失敗しました。'
       render :new
