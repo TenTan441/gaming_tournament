83fb60c422c7513e213745fd07727a65f176136d
Yoshiki <myutos44@gmail.com>
2019-09-12 16:07:29 +0000

    トップ画面雛形

--- config/environment.rb
+++ config/environment.rb
@@ -7,3 +7,7 @@ Rails.application.initialize!
 # ページネーションラベルの設定
 WillPaginate::ViewHelpers.pagination_options[:previous_label] = '前ページ'
 WillPaginate::ViewHelpers.pagination_options[:next_label] = '次ページ'
+
+# ChallongeのAPI使用設定
+Challonge::API.username = 'TenTan441'
+Challonge::API.key = 'RqayqZTRMe7TRXobnrkCGvlgh4u11gYzRpAt7i5D'
